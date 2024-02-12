18352bbfa79b08afa3c5c46d680dc8b95429a3a7
mo5e5 <malteoppermann@gmx.net>
2024-02-12 12:22:16 +0100

    navigation to evendetailscreen

--- app/src/main/java/com/example/boosthub/data/adapter/HomeEventAdapter.kt
+++ app/src/main/java/com/example/boosthub/data/adapter/HomeEventAdapter.kt
@@ -3,11 +3,13 @@ package com.example.boosthub.data.adapter
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.boosthub.R
 import com.example.boosthub.data.datamodel.Event
 import com.example.boosthub.databinding.ItemHomeEventBinding
+import com.example.boosthub.ui.HomeScreenFragmentDirections
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
@@ -36,5 +38,10 @@ class HomeEventAdapter(private val dataset: List<Event>) :
         holder.binding.itemHomeLocationInputMTV.text = event.location
 
         holder.binding.itemHomeWhatsUpInputMTV.text = event.whatsUp
+
+        holder.binding.itemHomeEventMCV.setOnClickListener {
+            val navController = holder.itemView.findNavController()
+            navController.navigate(HomeScreenFragmentDirections.actionHomeScreenFragmentToEventDetailScreenFragment(position))
+        }
     }
 }
--- app/src/main/java/com/example/boosthub/ui/EventDetailScreenFragment.kt
+++ app/src/main/java/com/example/boosthub/ui/EventDetailScreenFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import coil.load
 import com.example.boosthub.MainViewModel
 import com.example.boosthub.databinding.FragmentEventDetailScreenBinding
 
@@ -14,11 +16,18 @@ class EventDetailScreenFragment : Fragment() {
     private lateinit var binding: FragmentEventDetailScreenBinding
     private val viewModel: MainViewModel by activityViewModels()
 
+    private val args = EventDetailScreenFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentEventDetailScreenBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
