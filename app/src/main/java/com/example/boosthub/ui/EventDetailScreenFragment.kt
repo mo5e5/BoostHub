ec8808382da4b640fd5589f852946a95d5ce63d0
mo5e5 <malteoppermann@gmx.net>
2024-02-29 11:15:39 +0100

    added comments for MainViewModel and EventDetailScreenFragment

--- app/src/main/java/com/example/boosthub/ui/EventDetailScreenFragment.kt
+++ app/src/main/java/com/example/boosthub/ui/EventDetailScreenFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.PorterDuff
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
